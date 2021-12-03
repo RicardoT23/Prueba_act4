0ec43ca9901eec8646d77cab6349cc232e0ed638
ms_pr <ms_pr@DESKTOP-SLISA9N>
2021-12-03 12:34:00 -0700

    Cambio 1 Cambio 2

--- src/main/java/Ac4/Equipo.java
+++ src/main/java/Ac4/Equipo.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Ac4;
 
 /**
@@ -14,7 +11,8 @@ public class Equipo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola");
+        System.out.println("holaa");
     }
     
 }
